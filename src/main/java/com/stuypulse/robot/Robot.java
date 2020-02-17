3b4fa38b932d693ec2df8dceb7566919f21daa81
nlin21 <nlin21@stuy.edu>
2020-02-17 12:11:38 -0500

    Update 'chase' effect to 'shot' effect

--- src/main/java/com/stuypulse/robot/Robot.java
+++ src/main/java/com/stuypulse/robot/Robot.java
@@ -8,6 +8,7 @@
 package com.stuypulse.robot;
 
 import com.stuypulse.robot.util.LEDController;
+import com.stuypulse.stuylib.network.SmartNumber;
 
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -23,7 +24,7 @@ public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
 
   private RobotContainer m_robotContainer;
-
+  
   /**
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
@@ -33,6 +34,7 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
+
   }
 
   /**
@@ -50,6 +52,7 @@ public class Robot extends TimedRobot {
     // block in order for anything in the Command-based framework to work.
     CommandScheduler.getInstance().run();
     m_robotContainer.getLEDController().controlLEDs();
+
   }
 
   /**
